fdb68ea1157629816838d6f81f9857fb4b6ecd49
bashar-web3 <bashar.web3@outlook.com>
2023-08-03 17:55:55 +0800

    update backend url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,7 @@
 // import axios from 'axios';
 
-// const proBaseURL = 'http://192.168.0.115:8080';
-// const proBaseURL = 'https://scan.sama.network';
-const proBaseURL = 'http://16.162.44.95:8080';
+// const proBaseURL = 'http://16.162.44.95:8080';
+const proBaseURL = 'http://explorer.sigtacchain.com';
 
 // http://47.122.41.201:8000/api/apps
 
