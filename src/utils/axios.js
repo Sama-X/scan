063630bca36ce945cd6f10c04a8543144e90ad34
bashar-web3 <bashar.web3@outlook.com>
2023-08-03 15:37:30 +0800

    update:lq backend api

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,8 @@
 // import axios from 'axios';
 
 // const proBaseURL = 'http://192.168.0.115:8080';
-const proBaseURL = 'https://scan.sama.network';
+// const proBaseURL = 'https://scan.sama.network';
+const proBaseURL = 'http://16.162.44.95:8080';
 
 // http://47.122.41.201:8000/api/apps
 
